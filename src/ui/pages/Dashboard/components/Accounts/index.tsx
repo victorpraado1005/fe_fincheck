0231156451b52e58f8a8d81bc4f3bb61113e9ba8
Victor Prado <prado.victor1005@gmail.com>
2024-02-22 19:25:54 -0300

    feat: finalizando slider de banks accounts.

--- src/ui/pages/Dashboard/components/Accounts/index.tsx
+++ src/ui/pages/Dashboard/components/Accounts/index.tsx
@@ -1,10 +1,13 @@
 import 'swiper/css';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EyeIcon } from "../../../components/icons/EyeIcon";
+import { EyeIcon } from '../../../../components/icons/EyeIcon';
 import { AccountCard } from "./AccountCard";
 import { AccountsSliderNavigation } from './AccountsSliderNavigation';
+import { useAccountsController } from './useAccountsController';
 
 export function Accounts() {
+  const { sliderState, setSliderState, windowWidth } = useAccountsController();
+
   return (
     <div className="bg-teal-900 rounded-2xl h-full w-full md:p-10 px-4 py-8 flex flex-col">
       <div>
@@ -17,15 +20,24 @@ export function Accounts() {
         </div>
       </div>
 
-      <div className="flex-1 flex flex-col justify-end">
+      <div className="flex-1 flex flex-col justify-end mt-10 md:mt-0">
         <div>
           <Swiper
             spaceBetween={16}
-            slidesPerView={2.1}
+            slidesPerView={windowWidth >= 500 ? 2.1 : 1.15}
+            onSlideChange={swiper => {
+              setSliderState({
+                isBeginning: swiper.isBeginning,
+                isEnd: swiper.isEnd
+              })
+            }}
           >
             <div className="flex items-center justify-between mb-4" slot='container-start'>
               <strong className="text-white tracking-[-1px] text-lg">Minhas contas</strong>
-              <AccountsSliderNavigation />
+              <AccountsSliderNavigation
+                isBeginning={sliderState.isBeginning}
+                isEnd={sliderState.isEnd}
+              />
             </div>
 
             <SwiperSlide>
